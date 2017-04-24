0479b80b2b06b7821dfff608d3813768e21d401a
Alex Egg <eggie5@gmail.com>
2017-04-24 13:01:03 -0700

    * filter out users w/ less than 2 reveiws * random auc user sampling * adaptive learning rate

--- bpr.py
+++ bpr.py
@@ -21,7 +21,7 @@ def load_data(data_path):
     with open(data_path, 'r') as f:
         for line in f.readlines():
             reviews+=1
-            auid, asid, _ = line.split(",")
+            auid, asid, _= line.split(",")
             u, i = None, None
             
             if auid in users:
@@ -57,7 +57,14 @@ def load_data(data_path):
     # print "unfiltered len:",len(user_ratings)
     # print" filtered len:",len(filtered_user_ratings)
     
-    return max_u_id, max_i_id, user_ratings
+    #filter out users w/ less than X reviews
+    user_ratings_filtered = defaultdict(set)
+    for u,ids in user_ratings.items():
+      if len(ids)>1:
+        #keep
+        user_ratings_filtered[u]=ids
+    
+    return max_u_id, max_i_id, user_ratings_filtered
     
 
 
@@ -92,7 +99,8 @@ def generate_train_batch(user_ratings, user_ratings_test, item_count, batch_size
         t.append([u, i, j])
     return numpy.asarray(t)
 
-def generate_test_batch(user_ratings, user_ratings_test, item_count, users_size=512, items_size=512):
+#sample users but not items
+def generate_test_batch(user_ratings, user_ratings_test, item_count, users_size=512):
     '''
     for an user u and an item i rated by u, 
     generate pairs (u,i,j) for all item j which u has't rated
@@ -108,12 +116,12 @@ def generate_test_batch(user_ratings, user_ratings_test, item_count, users_size=
     for u in random.sample(user_ratings.keys(), users_size):
         t = []
         i = user_ratings_test[u]
-        for j in random.sample(range(1, item_count), items_size):
+        for j in range(1, item_count):
             if not (j in user_ratings[u]):
                 t.append([u, i, j])
         yield numpy.asarray(t) #returns a batch per user
         
-def bpr_mf(user_count, item_count, hidden_dim, learning_rate=0.1):
+def bpr_mf(user_count, item_count, hidden_dim, starter_learning_rate=0.1, regulation_rate = 0.0001):
   
     #model input
     u = tf.placeholder(tf.int32, [None])
@@ -149,36 +157,45 @@ def bpr_mf(user_count, item_count, hidden_dim, learning_rate=0.1):
             tf.reduce_sum(tf.multiply(u_emb, u_emb)), 
             tf.reduce_sum(tf.multiply(i_emb, i_emb)),
             tf.reduce_sum(tf.multiply(j_emb, j_emb))
+            #should reg the item bias term?
         ])
     
-    regulation_rate = 0.0001
+
     bprloss = regulation_rate * l2_norm - tf.reduce_mean(tf.log(tf.sigmoid(x))) #BPR loss
     
+    global_step = tf.Variable(0, trainable=False)
+    learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, 400, 0.8, staircase=True)
+                                               
     optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    train_op = optimizer.minimize(bprloss)
+    train_op = optimizer.minimize(bprloss, global_step=global_step)
     return u, i, j, mf_auc, bprloss, train_op
     
 #start
 
-data_path = os.path.join('data/amzn/', 'clothing_reviews.csv')
+data_path = os.path.join('data/amzn/', 'review_Women.csv')
 # data_path = os.path.join('data/amzn/', 'reviews-women-full.csv')
 user_count, item_count, user_ratings = load_data(data_path)
 
 user_ratings_test = generate_test(user_ratings)
 
 # 512*x=239290; x=400
-samples_count = 204800
+#UxIxJ = 30k x 8k x 8k = 
 batch_size=512
 batches=400
-epochs=10
+epochs=15
 K=20
 learning_rate=0.1
+regulation_rate = 0.1
+
+
     
 with tf.Graph().as_default(), tf.Session() as session:
     
-    print "running..."
-    u, i, j, mf_auc, bprloss, train_op = bpr_mf(user_count, item_count, K, learning_rate=learning_rate)
+    print "training..."
+    u, i, j, mf_auc, bprloss, train_op = bpr_mf(user_count, item_count, K, starter_learning_rate=learning_rate, regulation_rate=regulation_rate)
     session.run(tf.global_variables_initializer())
+    
+    
     for epoch in range(1, epochs+1):
       
         #ideally, I want to run the whole dataset each epoch, so adjust batches and batch size accordingly. 
@@ -187,7 +204,7 @@ with tf.Graph().as_default(), tf.Session() as session:
         for k in range(1,batches): # uniform batch samples from training set
             if(k%(batches/10)==0):
               print "iteration,batch: ",epoch,k
-              
+            
             uij = generate_train_batch(user_ratings, user_ratings_test, item_count, batch_size=batch_size)
 
             _bprloss, _ = session.run([bprloss, train_op], feed_dict={u:uij[:,0], i:uij[:,1], j:uij[:,2]})
@@ -196,21 +213,24 @@ with tf.Graph().as_default(), tf.Session() as session:
         print "epoch: ", epoch
         print "bpr_loss: ", _batch_bprloss / k
 
-        # user_count = 0
-        # auc_sum = 0.0
-        #
-        # # each batch will return only one user's auc
-        # test_samples = generate_test_batch(user_ratings, user_ratings_test, item_count, users_size=5000, items_size=1000)
-        # for t_uij in test_samples:
-        #     #parallelize this loop
-        #     user_auc, _test_bprloss = session.run([mf_auc, bprloss], feed_dict={u:t_uij[:,0], i:t_uij[:,1], j:t_uij[:,2]}
-        #                         )
-        #     user_count += 1
-        #     auc_sum += user_auc
-        #
-        # auc_mean = auc_sum / user_count
-        # print "test_loss: ", _test_bprloss, "test_auc: ", auc_mean
-        # print ""
+        user_count = 0
+        auc_values=[]
+        print "auc..."
+        # each batch will return only one user's auc
+    
+        #these samples need to be generated in parallel
+        test_samples = generate_test_batch(user_ratings, user_ratings_test, item_count, users_size=3000)
+        for t_uij in test_samples:
+        
+            #process: t_uij
+            user_count += 1
+            if user_count % 1000 == 0:
+              print "Current AUC mean (%s user samples): %0.5f" % (user_count, numpy.mean(auc_values))
+            user_auc = session.run(mf_auc, feed_dict={u:t_uij[:,0], i:t_uij[:,1], j:t_uij[:,2]})
+            auc_values.append(user_auc)
+
+        print "test_auc: ", numpy.mean(auc_values)
+        print ""
     
     summary_writer = tf.summary.FileWriter('log_simple_stats', session.graph)
     summary_writer.close()
@@ -220,3 +240,9 @@ with tf.Graph().as_default(), tf.Session() as session:
 #take n_votes random uij samples, where n_votes=278,677 is the number of obeservatiosn in teh dataset
 #but this impl is doing 5000*512=2,560,000 samples per epoch which is way too much data I think
 #so if I adjust keep batch size 512 constant, k would be 278677/512=544==600
+
+#512/400 - 8:33-8:45 test_auc:  0.554972909522
+#4096/50 - 8:49-9:04 test_auc:  0.513462866851
+
+# run w/o GPU
+# CUDA_VISIBLE_DEVICES="" time python bpr.py
