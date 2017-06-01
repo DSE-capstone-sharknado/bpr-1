af489d1a4a848991ff6899f32da380e34cb10090
Alex Egg <eggie5@gmail.com>
2017-06-01 14:49:17 -0700

    cleanup whitespace

--- vbpr.py
+++ vbpr.py
@@ -21,7 +21,7 @@ def generate_val_and_test(user_ratings):
         user_test[u] = samples[0]
         user_val[u] = samples[1]
     return user_val, user_test
-    
+
 def uniform_sample_batch(train_ratings, val_ratings, test_ratings, item_count, image_features, sample_count=400, batch_size=512):
     for i in xrange(sample_count):
         t = []
@@ -33,18 +33,18 @@ def uniform_sample_batch(train_ratings, val_ratings, test_ratings, item_count, i
             i = random.sample(train_ratings[u], 1)[0]
             while i == test_ratings[u] or i==val_ratings[u]: #make sure i is not in the test or val set
                 i = random.sample(train_ratings[u], 1)[0]
-                
+            
             j = random.randint(0, item_count)
             while j in train_ratings[u]: #make sure j is not in users reviews (ie it is negative)
                 j = random.randint(0, item_count)
-                
+            
             #sometimes there will not be an image for given item i or j
-            try: 
+            try:
               image_features[i]
               image_features[j]
             except KeyError:
               continue  #if so, skip this item
-              
+            
             t.append([u, i, j])
             iv.append(image_features[i])
             jv.append(image_features[j])
@@ -61,7 +61,7 @@ def test_batch_generator_by_user(train_ratings, test_ratings, item_count, image_
         #check if we have an image for i, sometimes we dont...
         if i not in image_features:
           continue
-          
+        
         #filter for cold start
         if cold_start and item_dist[i] > 5:
           continue
@@ -70,9 +70,9 @@ def test_batch_generator_by_user(train_ratings, test_ratings, item_count, image_
             j = random.randint(1, item_count)
             if j != test_ratings[u] and not (j in train_ratings[u]):
                 # find negative item not in train or test set
-
+                
                 #sometimes there will not be an image for given product
-                try: 
+                try:
                   image_features[i]
                   image_features[j]
                 except KeyError:
@@ -81,10 +81,10 @@ def test_batch_generator_by_user(train_ratings, test_ratings, item_count, image_
                 t.append([u, i, j])
                 ilist.append(image_features[i])
                 jlist.append(image_features[j])
-                
-        yield numpy.asarray(t), numpy.vstack(tuple(ilist)), numpy.vstack(tuple(jlist))
         
-def vbpr(user_count, item_count, hidden_dim=20, hidden_img_dim=128, 
+        yield numpy.asarray(t), numpy.vstack(tuple(ilist)), numpy.vstack(tuple(jlist))
+
+def vbpr(user_count, item_count, hidden_dim=20, hidden_img_dim=128,
          learning_rate=0.001,
           l2_regulization=0.001,
           bias_regulization=0.001,
@@ -103,20 +103,20 @@ def vbpr(user_count, item_count, hidden_dim=20, hidden_img_dim=128,
     iv = tf.placeholder(tf.float32, [None, 4096])
     jv = tf.placeholder(tf.float32, [None, 4096])
     
-    user_emb_w = tf.get_variable("user_emb_w", [user_count+1, hidden_dim], 
+    user_emb_w = tf.get_variable("user_emb_w", [user_count+1, hidden_dim],
                                 initializer=tf.random_normal_initializer(0, 0.1))
     user_img_w = tf.get_variable("user_img_w", [user_count+1, hidden_img_dim],
                                 initializer=tf.random_normal_initializer(0, 0.1)) #theta_u
-    item_emb_w = tf.get_variable("item_emb_w", [item_count+1, hidden_dim], 
+    item_emb_w = tf.get_variable("item_emb_w", [item_count+1, hidden_dim],
                                 initializer=tf.random_normal_initializer(0, 0.1))
-    item_b = tf.get_variable("item_b", [item_count+1, 1], 
+    item_b = tf.get_variable("item_b", [item_count+1, 1],
                                 initializer=tf.constant_initializer(0.0))
     visual_bias = tf.get_variable("visual_bias", [1, 4096], initializer=tf.constant_initializer(0.0))
     
-    img_emb_w = tf.get_variable("image_embedding_weights", [4096, hidden_img_dim], 
+    img_emb_w = tf.get_variable("image_embedding_weights", [4096, hidden_img_dim],
                                initializer=tf.random_normal_initializer(0, 0.1))
-
                         
+    
     
     #lookup the latent factors by user and id
     u_emb = tf.nn.embedding_lookup(user_emb_w, u)
@@ -126,10 +126,10 @@ def vbpr(user_count, item_count, hidden_dim=20, hidden_img_dim=128,
     i_b = tf.nn.embedding_lookup(item_b, i)
     j_emb = tf.nn.embedding_lookup(item_emb_w, j)
     j_b = tf.nn.embedding_lookup(item_b, j)
-    
 
                                
 
+    
     # MF predict: u_i > u_j
     theta_i = tf.matmul(iv, img_emb_w) # (f_i * E), eq. 3 1xK2 x 4096xK2 => 1xK2 #plot these on 2d scatter
     theta_j = tf.matmul(jv, img_emb_w) # (f_j * E), eq. 3
@@ -137,14 +137,14 @@ def vbpr(user_count, item_count, hidden_dim=20, hidden_img_dim=128,
     xuj = j_b + tf.reduce_sum(tf.multiply(u_emb, j_emb), 1, keep_dims=True) + tf.reduce_sum(tf.multiply(u_img, theta_j), 1, keep_dims=True) + tf.reduce_sum(tf.multiply(visual_bias, jv), 1, keep_dims=True)
     xuij = xui - xuj
     #
-
+    
     # auc score is used in test/cv
     # reduce_mean is reasonable BECAUSE
     # all test (i, j) pairs of one user is in ONE batch
     auc = tf.reduce_mean(tf.to_float(xuij > 0))
-
+    
     l2_norm = tf.add_n([
-            l2_regulization * tf.reduce_sum(tf.multiply(u_emb, u_emb)), 
+            l2_regulization * tf.reduce_sum(tf.multiply(u_emb, u_emb)),
             image_regulization * tf.reduce_sum(tf.multiply(u_img, u_img)),
             l2_regulization * tf.reduce_sum(tf.multiply(i_emb, i_emb)),
             l2_regulization * tf.reduce_sum(tf.multiply(j_emb, j_emb)),
@@ -153,7 +153,7 @@ def vbpr(user_count, item_count, hidden_dim=20, hidden_img_dim=128,
             bias_regulization * tf.reduce_sum(tf.multiply(j_b, j_b)),
             visual_bias_regulization * tf.reduce_sum(tf.multiply(visual_bias,visual_bias))
         ])
-
+    
     loss = l2_norm - tf.reduce_mean(tf.log(tf.sigmoid(xuij)))
     #train_op =  tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(loss)
     #train_op =  tf.train.AdagradOptimizer(learning_rate).minimize(loss)
@@ -161,7 +161,7 @@ def vbpr(user_count, item_count, hidden_dim=20, hidden_img_dim=128,
     # train_op = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
     print "Hyper-parameters: K=%d, K2=%d, lr=%f, l2r=%f"%(hidden_dim, hidden_img_dim, learning_rate, l2_regulization)
     return u, i, j, iv, jv, loss, auc, train_op
-    
+
 
 
 print "Loading dataset..."
@@ -177,11 +177,11 @@ item_count = len(item_users)
 reviews_count = len(reviews_all)
 print user_count,item_count,reviews_count
 
-
   
+
 images_path = "data/amzn/image_features_Women.b"
-image_features = load_image_features(images_path, items)    
-    
+image_features = load_image_features(images_path, items)
+
 print "extracted image feature count: ",len(image_features)
 #4096 floats * 24bytes = 98,304 bytes/feature = 98KB
 #4096 * 24 * 302,047 = 29 GB
@@ -216,8 +216,8 @@ with tf.Graph().as_default(), tf.Session() as session:
         epoch_durations.append(time.time() - epoch_start)
         
         if epoch % 5 != 0:
-          print "epoch: %d (%.2fs)"%(epoch, np.mean(epoch_durations) )        
-          continue 
+          print "epoch: %d (%.2fs)"%(epoch, np.mean(epoch_durations) )
+          continue
         
         #train eval
         val_auc_vals=[]
@@ -240,7 +240,7 @@ with tf.Graph().as_default(), tf.Session() as session:
           break
         else:
           print
-        
+    
     #restore best model from checkpoint
     saver.restore(session, "logs/")
     #test auc
@@ -248,8 +248,8 @@ with tf.Graph().as_default(), tf.Session() as session:
     for d, fi, fj in test_batch_generator_by_user(train_ratings, test_ratings, item_count, image_features, sample_size=3000):
         _auc = session.run(auc, feed_dict={u:d[:,0], i:d[:,1], j:d[:,2], iv:fi, jv:fj})
         test_auc_vals.append(_auc)
-    test_auc = np.mean(test_auc_vals) 
-    print "Best model iteration %d, test: %.3f, val: %.3f"%(best_iter,test_auc,best_auc) 
+    test_auc = np.mean(test_auc_vals)
+    print "Best model iteration %d, test: %.3f, val: %.3f"%(best_iter,test_auc,best_auc)
     
     
     #cold auc
@@ -258,10 +258,10 @@ with tf.Graph().as_default(), tf.Session() as session:
         _auc = session.run(auc, feed_dict={u:d[:,0], i:d[:,1], j:d[:,2], iv:fi, jv:fj})
         cold_auc_vals.append(_auc)
     print "cold auc: %.2f"%(np.mean(cold_auc_vals) )
-        
 
      
         
+
 
 # nohup time python -u vbpr.py > vbpr3-test005.log 2>&1 &
 
