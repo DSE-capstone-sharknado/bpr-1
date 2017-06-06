ca01c552cf456af951e99c2eba90479a740c4aa8
Alex Egg <eggie5@gmail.com>
2017-06-06 15:11:18 -0700

    evaluator for BPR models

---
+++ train.py
@@ -0,0 +1,144 @@
+from vbpr import VBPR
+from bpr import BPR
+from hbpr import HBPR
+from corpus import Corpus
+import argparse
+import os
+import sys
+import tensorflow as tf
+import sampling
+import numpy as np
+
+def main(_):
+  corpus = Corpus()
+  corpus.load_data(FLAGS.reviews_path, FLAGS.images_path, 5, 0);
+  
+  sampler = sampling.Uniform()
+  
+  model = None
+  if FLAGS.model == "BPR":
+    model = BPR(tf.Session(), corpus, sampler, FLAGS.K, FLAGS.reg, FLAGS.bias_reg)
+  elif FLAGS.model == "VBPR": 
+    model = VBPR(tf.Session(), corpus, sampler, FLAGS.K, FLAGS.K2, FLAGS.reg, FLAGS.bias_reg)
+  elif FLAGS.model == "HBPR":
+    model = HBPR(tf.Session(), corpus, sampler, FLAGS.K, FLAGS.K2, FLAGS.reg, FLAGS.bias_reg)
+  else:
+    raise Exception("Could not find model %s"%FLAGS.model)
+  
+  evaluate(model)
+
+def evaluate(model):
+  epoch_durations = []
+  best_auc=-1
+  best_iter=-1
+  for iteration, duration, train_loss in model.train(FLAGS.max_iterations, FLAGS.batch_size, FLAGS.batch_count ):
+    epoch_durations.append(duration)
+    
+    if iteration % 5 != 0:
+      print "iteration: %d (%.2fs), train loss: %.2f"%(iteration, np.mean(epoch_durations), train_loss )
+      continue
+    
+    #train eval
+    val_auc, val_loss = model.evaluate(model.val_ratings,  sample_size=1000)
+    print "iteration: %d (%.2fs), train loss: %.2f, val loss: %.2f, val auc: %.2f"%(iteration, np.mean(epoch_durations), train_loss ,val_loss, val_auc ),
+    
+    #early termination/checks for convergance
+    if val_auc > best_auc:
+      best_auc = val_auc
+      best_iter = iteration
+      print "*"
+      model.save()
+    elif val_auc < best_iter and iteration >= best_iter+21: #overfitting
+      print "Overfitted. Exiting..."
+      break
+    else:
+      print
+  
+  #restore best model from checkpoint
+  model.restore()
+  #test auc
+  test_auc, test_loss = model.evaluate(model.test_ratings, sample_size=100)
+  print "Best model iteration %d, test: %.3f, val: %.3f"%(best_iter, test_auc, best_auc)
+
+  
+  #cold auc
+  cold_auc, cold_loss = model.evaluate(model.test_ratings, sample_size=100, cold_start=True)
+  print "cold auc: %.2f"%(cold_auc)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      '--model',
+      type=str,
+      default="BPR",
+      help='Which model to evaluate?: bpr, vbpr or hbpr'
+  )
+  parser.add_argument(
+      '--K',
+      type=int,
+      default=20,
+      help='Dimension of Latent Factors'
+  )
+  parser.add_argument(
+      '--K2',
+      type=int,
+      default=20,
+      help='Dimension of Visual Factors'
+  )
+  parser.add_argument(
+      '--reg',
+      type=float,
+      default=10,
+      help='L2 Regularization constant for latent factors'
+  )
+  parser.add_argument(
+      '--bias_reg',
+      type=float,
+      default=0.01,
+      help='L2 regularization constant for item i,j bias vectors'
+  )
+  parser.add_argument(
+      '--max_iterations',
+      type=int,
+      default=2000,
+      help='Number of steps to run trainer.'
+  )
+  parser.add_argument(
+      '--batch_size',
+      type=int,
+      default=512,
+      help='Batch size. Must divide evenly into the dataset sizes.'
+  )
+  
+  parser.add_argument(
+      '--batch_count',
+      type=int,
+      default=400,
+      help='Batch Count. How many samples of batch_size per GD iteration. Arbitrary amount as sampling is random...'
+  )
+  parser.add_argument(
+      '--images_path',
+      type=str,
+      default="data/amzn/image_features_Women.b",
+      help='Path to image features file'
+  )
+  parser.add_argument(
+      '--reviews_path',
+      type=str,
+      default="data/amzn/reviews_Women_ALL_scraped.csv",
+      help='Path to reviews file'
+  )
+  parser.add_argument(
+      '--name',
+      type=str,
+      default="BPR",
+      help='The name of this trainging session'
+  )
+  
+  FLAGS, unparsed = parser.parse_known_args()
+  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+
+
+# nohup time python -u vbpr.py > vbpr3-test005.log 2>&1 &
+
+# nohup time ./train ../tf-bpr/data/amzn/reviews_Women.txt ../tf-bpr/data/amzn/image_features_Women.b 10 10 na 0.01 9.5 0.01 na 20 "women" > logs/vbpr-test001.log 2>&1 &
